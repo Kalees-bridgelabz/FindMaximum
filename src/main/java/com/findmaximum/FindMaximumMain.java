1e0889f2c9d7e8fb5a32003b1d6f4dd584583b61
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-03-14 12:58:07 +0530

    UC2: repeat with the similar 3 Test cases in

--- src/main/java/com/findmaximum/FindMaximumMain.java
+++ src/main/java/com/findmaximum/FindMaximumMain.java
@@ -3,14 +3,10 @@ package com.findmaximum;
 import java.util.Scanner;
 
 /**
- * UC1 - method to test the maximum number
+ * UC2 - repeat with the similar 3 Test cases in
  *
  * Requirements:
-     *   - Given Max Number at
-     *   - 1st Position return the
-     *   - Same Number
-     *   - TC 1.1
-     *   - Ensure the test data has Max Number in
+     *   (See PDF)
  *
  * @author  Kalees-bridgelabz
  * @version 1.0
@@ -20,11 +16,9 @@ public class FindMaximumMain {
     public static void main(String[] args) {
         System.out.println("Welcome to FindMaximum");
         
-        System.out.println("\n=== UC1: method to test the maximum number ===");
-        // Find Maximum using Generics / Comparable
-        Integer a = 56, b = 30, c = 70;
-        Integer max = (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) ? a
-                    : (b.compareTo(c) >= 0) ? b : c;
-        System.out.println("Max of " + a + ", " + b + ", " + c + " = " + max);
+        System.out.println("\n=== UC2: repeat with the similar 3 Test cases in ===");
+        // TODO: Implement UC2
+        // (see PDF)
+        System.out.println("UC2 - Implementation pending");
     }
 }
