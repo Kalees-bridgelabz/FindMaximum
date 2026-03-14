37591e32c3884a59836e7a0b2d56fc27a8de4598
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-03-14 12:58:28 +0530

    UC5: internally called from testMaximum

--- src/main/java/com/findmaximum/FindMaximumMain.java
+++ src/main/java/com/findmaximum/FindMaximumMain.java
@@ -3,14 +3,10 @@ package com.findmaximum;
 import java.util.Scanner;
 
 /**
- * UC4 - - Use Options and use Sorting
+ * UC5 - internally called from testMaximum
  *
  * Requirements:
-     *   - Extend the max
-     *   - method to also print
-     *   - the max to std out
-     *   - using Generic Method
-     *   - Write printMax Generic Method which is
+     *   (See PDF)
  *
  * @author  Kalees-bridgelabz
  * @version 1.0
@@ -20,8 +16,11 @@ public class FindMaximumMain {
     public static void main(String[] args) {
         System.out.println("Welcome to FindMaximum");
         
-        System.out.println("\n=== UC4: - Use Options and use Sorting ===");
-        // TODO: Implement sort logic - ascending/alphabetical order
-        System.out.println("Sorted successfully!");
+        System.out.println("\n=== UC5: internally called from testMaximum ===");
+        // Find Maximum using Generics / Comparable
+        Integer a = 56, b = 30, c = 70;
+        Integer max = (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) ? a
+                    : (b.compareTo(c) >= 0) ? b : c;
+        System.out.println("Max of " + a + ", " + b + ", " + c + " = " + max);
     }
 }
