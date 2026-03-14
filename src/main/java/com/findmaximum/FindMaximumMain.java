384a74df1c4685680d74dac426f6548e09c8571b
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-03-14 12:58:01 +0530

    UC1: method to test the maximum number

--- src/main/java/com/findmaximum/FindMaximumMain.java
+++ src/main/java/com/findmaximum/FindMaximumMain.java
@@ -1,7 +1,30 @@
 package com.findmaximum;
 
+import java.util.Scanner;
+
+/**
+ * UC1 - method to test the maximum number
+ *
+ * Requirements:
+     *   - Given Max Number at
+     *   - 1st Position return the
+     *   - Same Number
+     *   - TC 1.1
+     *   - Ensure the test data has Max Number in
+ *
+ * @author  Kalees-bridgelabz
+ * @version 1.0
+ */
 public class FindMaximumMain {
+
     public static void main(String[] args) {
         System.out.println("Welcome to FindMaximum");
+        
+        System.out.println("\n=== UC1: method to test the maximum number ===");
+        // Find Maximum using Generics / Comparable
+        Integer a = 56, b = 30, c = 70;
+        Integer max = (a.compareTo(b) >= 0 && a.compareTo(c) >= 0) ? a
+                    : (b.compareTo(c) >= 0) ? b : c;
+        System.out.println("Max of " + a + ", " + b + ", " + c + " = " + max);
     }
 }
