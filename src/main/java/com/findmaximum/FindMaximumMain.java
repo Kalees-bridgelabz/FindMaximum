ff01eb2eeca3f08fcf6d4e5aaf31d08d6d211a0c
Kalees-bridgelabz <kaleeswaran.eswaran@bridgelabz.com>
2026-03-14 12:58:22 +0530

    UC4: - Use Options and use Sorting

--- src/main/java/com/findmaximum/FindMaximumMain.java
+++ src/main/java/com/findmaximum/FindMaximumMain.java
@@ -3,10 +3,14 @@ package com.findmaximum;
 import java.util.Scanner;
 
 /**
- * UC3 - - E.g. Apple Peach Banana
+ * UC4 - - Use Options and use Sorting
  *
  * Requirements:
-     *   (See PDF)
+     *   - Extend the max
+     *   - method to also print
+     *   - the max to std out
+     *   - using Generic Method
+     *   - Write printMax Generic Method which is
  *
  * @author  Kalees-bridgelabz
  * @version 1.0
@@ -16,9 +20,8 @@ public class FindMaximumMain {
     public static void main(String[] args) {
         System.out.println("Welcome to FindMaximum");
         
-        System.out.println("\n=== UC3: - E.g. Apple Peach Banana ===");
-        // TODO: Implement UC3
-        // (see PDF)
-        System.out.println("UC3 - Implementation pending");
+        System.out.println("\n=== UC4: - Use Options and use Sorting ===");
+        // TODO: Implement sort logic - ascending/alphabetical order
+        System.out.println("Sorted successfully!");
     }
 }
